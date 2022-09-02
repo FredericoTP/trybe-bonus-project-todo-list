ae08280db1ed69a17a8e1c52e41ec81b461ea7d2
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-02 13:29:36 -0300

    Requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 const mainContent = document.getElementById('main-content');
 const sectionList = document.getElementById('list');
+const sectionAddItem = document.getElementById('add-item');
 
-function createOrderedList() {
-  const orderedList = document.createElement('ol');
-  orderedList.id = 'lista-tarefas';
-  sectionList.appendChild(orderedList);
+function createOrderedList(local, id) {
+  const createOrderedList = document.createElement('ol');
+  createOrderedList.id = id;
+  local.appendChild(createOrderedList);
 }
 
-createOrderedList();
+createOrderedList(sectionList, 'lista-tarefas');
+
+function createButton(local, id) {
+  const createButton = document.createElement('button');
+  createButton.id = id;
+  local.appendChild(createButton);
+}
+
+createButton(sectionAddItem, 'criar-tarefa');
+
+const buttonCriarTarefa = document.getElementById('criar-tarefa');
+buttonCriarTarefa.innerText = 'Criar tarefa';
+
+function createLi() {
+  const inputText = document.getElementById('texto-tarefa').value;
+  const createLi = document.createElement('li');
+  const orderedList = document.getElementById('lista-tarefas');
+  createLi.innerText = inputText;
+  orderedList.appendChild(createLi);
+}
+
+function buttonCreateLi() {
+  const button = document.getElementById('criar-tarefa');
+  button.addEventListener('click', function(event) {
+    event.target = createLi();
+    event.target = document.getElementById('texto-tarefa').value = '';
+  });
+}
+
+buttonCreateLi()
